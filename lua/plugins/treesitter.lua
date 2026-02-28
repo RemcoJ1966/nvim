07625919ac9d20cb252ec4f8203e7e9f6072c804
Remco Janssen <remcoj@xs4all.nl>
2026-02-28 13:55:31 +0800

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,30 @@ return {
     build = ':TSUpdate',
     config = function()
         local treesitter = require("nvim-treesitter")
-        treesitter.setup()
-        treesitter.install { 'lua', 'rust' }
+        treesitter.setup {
+            indent = { enable = true },
+            sync_install = false,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+        }
+        treesitter.install {
+            "lua",
+            "rust",
+            "c",
+            "cpp",
+            "c_sharp",
+            "gitcommit",
+            "gitignore",
+            "html",
+            "css",
+            "markdown",
+            "bash",
+            "ssh_config",
+            "xml",
+            "yaml",
+            "json",
+        }
     end,
 }
